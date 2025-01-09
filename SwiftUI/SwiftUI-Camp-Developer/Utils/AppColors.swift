8c4da4836a4b908fa104fd936155c08eaa37dff5
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-01-09 23:32:35 +0300

    add: Pre setup architecture created

---
+++ SwiftUI/SwiftUI-Camp-Developer/Utils/AppColors.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct AppColors {
+    // Button Colors
+    let primaryButton = Color(.black)
+    let secondaryButton = Color("#747474")
+    let boxBackgroundButton = Color("#F0F3FB")
+
+    // Text Colors
+
+    // Background Colors
+
+    // Border Colors
+    let portfolioBorder = Color("#D8DADC")
+
+    // Shadow Colors
+}
